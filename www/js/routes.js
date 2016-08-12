9d87299608b63f747a8dcc683eaaa967af93d9cc
Colin Waddell <mrcolin@gmail.com>
2016-08-12 23:26:53 +0100

    added custom directive to allow for double clicking on ios

--- www/js/routes.js
+++ www/js/routes.js
@@ -202,4 +202,34 @@ angular.module('app.routes', [])
           });
       }
   };
+})
+
+// Need custom directive for ios
+.directive('onDoubleClick', function ($timeout) {
+  return {
+  restrict: 'A',
+  link: function ($scope, $elm, $attrs) {
+    var clicks = 0;
+    var lastClick = new Date();
+      $elm.bind('click', function (evt) {
+          var dateDiff = new Date() - lastClick;
+          if (dateDiff > 300) { // 300 ms
+            clicks = 0;
+          }
+          lastClick = new Date();
+          clicks++;
+          if (clicks == 1) {
+            $timeout(function () {
+                if (clicks == 1) {
+                    //....
+                } else {
+                  $scope.$apply(function () {
+                    $scope.$eval($attrs.onDoubleClick);
+                  });
+                }
+            }, 300);
+          }
+      });
+    }
+  };
 });
